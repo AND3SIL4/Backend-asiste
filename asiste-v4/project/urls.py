d6e5fc734ef852e057554d1514bc9cee9bbe46d8
AND3SIL4 <afsilva6236@misena.edu.co>
2023-10-03 21:50:18 -0500

    ADD : adicion de comentarios de rutas para documentacion de la API

--- asiste-v4/project/urls.py
+++ asiste-v4/project/urls.py
@@ -11,7 +11,12 @@ urlpatterns = [
     path('users/', include('apps.users.urls')),
     path('asistencia/', include('apps.asistencia.urls')),
     
+    ## Desacargar documentacion de la API en formato .yaml
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    ## Cargar documentacion de la API visualmente y en el navegador
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    ## Prueba de endpoints y respuestas en formato JSON con key: value
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
